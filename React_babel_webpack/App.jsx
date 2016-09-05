c77d9f6035d0cde99e67df45af1c98b42de4f0ca
Gabrielle Bastien <gabrielle.bastien1@gmail.com>
2016-09-05 09:56:21 -0400

    added state and props simple examples, and component hierarchy

--- React_babel_webpack/App.jsx
+++ React_babel_webpack/App.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
 class App extends React.Component {
+
+	constructor(props) {
+      super(props);
+		
+      this.state = {
+         header: "Header from state",
+         "content": "Content from App state..."
+      }
+   }
+
+	render() {
+
+		var myStyle = {
+			backgroundColor: '#E5E5E5',
+			height: 30,
+			width: 200
+		}
+
+		return (
+			<div>
+				<h2 style = {myStyle}> {this.state.header}</h2>
+				<Content contentText = {this.state.content}/>
+			</div>
+		);
+	}
+}
+
+class Content extends React.Component {
 	render() {
 		return (
-			<div> Hello world!!!</div>
+			<div> {this.props.contentText} </div>
 		);
 	}
 }
